cebd3f2cb29ee9f6f7cf9245f06656bea63fb907
Mitar <mitar.git@tnode.com>
2017-06-07 23:06:35 -0700

    Parallelize DNS resolving.

--- exit.py
+++ exit.py
@@ -2,6 +2,7 @@
 
 import ipaddress
 import logging
+import multiprocessing
 import pyjsparser
 import socket
 from urllib.parse import urlparse
@@ -10,106 +11,110 @@ from urllib.request import urlopen
 logger = logging.getLogger(__file__)
 
 PROXY_PAC_URL = 'http://proxy.lib.berkeley.edu/proxy.pac'
+PARALLELISM = 10
 
-with urlopen(PROXY_PAC_URL) as request:
-  proxy_pac = request.read().decode('utf8')
+def resolve_domain(hostname):
+  result = []
 
-parsed_proxy_pac = pyjsparser.PyJsParser().parse(proxy_pac)
+  try:
+    for family, type, proto, canonname, sockaddr in socket.getaddrinfo(host=hostname, port=None, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP):
+      if family == socket.AF_INET:
+        result.append(ipaddress.IPv4Network(sockaddr[0]))
+      elif family == socket.AF_INET6:
+        result.append(ipaddress.IPv6Network(sockaddr[0]))
+  except socket.gaierror as error:
+    logger.info("Cannot resolve '{hostname}': {error}".format(hostname=hostname, error=error))
 
-def find_assignment_to(parsed, variable_name):
-  type = parsed['type']
-  if type in ('Program', 'BlockStatement'):
-    for body in parsed['body']:
-      found = find_assignment_to(body, variable_name)
-      if found:
-        return found
-  elif type in ('FunctionDeclaration', 'WhileStatement'):
-    return find_assignment_to(parsed['body'], variable_name)
-  elif type == 'IfStatement':
-    return find_assignment_to(parsed['consequent'], variable_name) or (parsed['alternate'] and find_assignment_to(parsed['alternate'], variable_name))
-  elif type == 'VariableDeclaration':
-    for declaration in parsed['declarations']:
-      if declaration['id']['name'] == variable_name:
-        return declaration['init']
-  elif type == 'ExpressionStatement':
-    if parsed['expression']['operator'] == '=' and parsed['expression']['left']['name'] == variable_name:
-      return parsed['expression']['right']
-  elif type in ('EmptyStatement', 'ReturnStatement'):
-    return
-  else:
-    logger.warn("Unknown type {type}.".format(type=type))
+  return result
 
-def convert_value(parsed):
-  if parsed is None:
-    return None
+if __name__ == '__main__':
+  with urlopen(PROXY_PAC_URL) as request:
+    proxy_pac = request.read().decode('utf8')
 
-  type = parsed['type']
-  if type == 'ArrayExpression':
-    return [convert_value(element) for element in parsed['elements']]
-  if type == 'Literal':
-    return parsed['value']
-  else:
-    logger.warn("Unknown type {type}.".format(type=type))
-    print(parsed)
+  parsed_proxy_pac = pyjsparser.PyJsParser().parse(proxy_pac)
 
-ips_to_never_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'ips_to_never_proxy'))
-shexps_to_never_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'shexps_to_never_proxy'))
-domains_to_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'domains_to_proxy'))
-shexps_to_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'shexps_to_proxy'))
-
-def rejected_addresses():
-  tor_exit_policy_reject_ipv4 = []
-  tor_exit_policy_reject_ipv6 = []
-
-  already_added_domains = []
-  def add_domain_to_policy(hostname):
-    if hostname in already_added_domains:
+  def find_assignment_to(parsed, variable_name):
+    type = parsed['type']
+    if type in ('Program', 'BlockStatement'):
+      for body in parsed['body']:
+        found = find_assignment_to(body, variable_name)
+        if found:
+          return found
+    elif type in ('FunctionDeclaration', 'WhileStatement'):
+      return find_assignment_to(parsed['body'], variable_name)
+    elif type == 'IfStatement':
+      return find_assignment_to(parsed['consequent'], variable_name) or (parsed['alternate'] and find_assignment_to(parsed['alternate'], variable_name))
+    elif type == 'VariableDeclaration':
+      for declaration in parsed['declarations']:
+        if declaration['id']['name'] == variable_name:
+          return declaration['init']
+    elif type == 'ExpressionStatement':
+      if parsed['expression']['operator'] == '=' and parsed['expression']['left']['name'] == variable_name:
+        return parsed['expression']['right']
+    elif type in ('EmptyStatement', 'ReturnStatement'):
       return
+    else:
+      logger.warn("Unknown type {type}.".format(type=type))
 
-    try:
-      for family, type, proto, canonname, sockaddr in socket.getaddrinfo(host=hostname, port=None, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP):
-        if family == socket.AF_INET:
-          tor_exit_policy_reject_ipv4.append(ipaddress.IPv4Network(sockaddr[0]))
-        elif family == socket.AF_INET6:
-          tor_exit_policy_reject_ipv6.append(ipaddress.IPv6Network(sockaddr[0]))
-    except socket.gaierror as error:
-      logger.info("Cannot resolve '{hostname}': {error}".format(hostname=hostname, error=error))
+  def convert_value(parsed):
+    if parsed is None:
+      return None
 
-    already_added_domains.append(hostname)
+    type = parsed['type']
+    if type == 'ArrayExpression':
+      return [convert_value(element) for element in parsed['elements']]
+    if type == 'Literal':
+      return parsed['value']
+    else:
+      logger.warn("Unknown type {type}.".format(type=type))
+      print(parsed)
 
-  # We add both entries to proxy and entries not to proxy to the reject policy.
-  # We do not want to allow tor exit to any of those Internet addresses.
+  ips_to_never_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'ips_to_never_proxy'))
+  shexps_to_never_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'shexps_to_never_proxy'))
+  domains_to_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'domains_to_proxy'))
+  shexps_to_proxy = convert_value(find_assignment_to(parsed_proxy_pac, 'shexps_to_proxy'))
 
-  for address, mask in ips_to_never_proxy:
-    # Two-tuple form for the address constructor parameter was added in Python 3.5.
-    tor_exit_policy_reject_ipv4.append(ipaddress.IPv4Network((address, mask)))
+  def rejected_addresses():
+    tor_exit_policy_reject_ipv4 = []
+    tor_exit_policy_reject_ipv6 = []
 
-  for shexps in shexps_to_never_proxy:
-    add_domain_to_policy(urlparse(shexps).hostname)
+    # We add both entries to proxy and entries not to proxy to the reject policy.
+    # We do not want to allow tor exit to any of those Internet addresses.
 
-  for domain in domains_to_proxy:
-    add_domain_to_policy(domain)
+    for address, mask in ips_to_never_proxy:
+      # Two-tuple form for the address constructor parameter was added in Python 3.5.
+      tor_exit_policy_reject_ipv4.append(ipaddress.IPv4Network((address, mask)))
 
-  for shexps in shexps_to_proxy:
-    add_domain_to_policy(urlparse(shexps).hostname)
+    domains = []
+    for shexps in shexps_to_never_proxy:
+      domains.append(urlparse(shexps).hostname)
+    for shexps in shexps_to_proxy:
+      domains.append(urlparse(shexps).hostname)
+    domains += domains_to_proxy
 
-  for address in sorted(ipaddress.collapse_addresses(tor_exit_policy_reject_ipv4)):
-    # Some DNS entries resolve to invalid addresses. Filter some of them out.
-    if address.is_multicast or address.is_private or address.is_reserved or address.is_loopback or address.is_link_local:
-      continue
-    yield address
+    with multiprocessing.Pool(PARALLELISM) as pool:
+      addresses = [address for resolved_addresses in pool.imap_unordered(resolve_domain, domains) for address in resolved_addresses]
 
-  for address in sorted(ipaddress.collapse_addresses(tor_exit_policy_reject_ipv6)):
-    # Some DNS entries resolve to invalid addresses. Filter some of them out.
-    if address.is_multicast or address.is_private or address.is_reserved or address.is_loopback or address.is_link_local:
-      continue
-    yield address
+    tor_exit_policy_reject_ipv4 += [address for address in addresses if isinstance(address, ipaddress.IPv4Network)]
+    tor_exit_policy_reject_ipv6 += [address for address in addresses if isinstance(address, ipaddress.IPv6Network)]
 
-for address in rejected_addresses():
-  if isinstance(address, ipaddress.IPv4Network):
-    print('reject {address}:*'.format(address=address))
-  elif isinstance(address, ipaddress.IPv6Network):
-    print('reject6 [{address}]/{mask}:*'.format(address=address.network_address, mask=address.prefixlen))
-  else:
-    raise TypeError("Unknown address type: {address}".format(address=address))
+    for address in sorted(ipaddress.collapse_addresses(tor_exit_policy_reject_ipv4)):
+      # Some DNS entries resolve to invalid addresses. Filter some of them out.
+      if address.is_multicast or address.is_private or address.is_reserved or address.is_loopback or address.is_link_local:
+        continue
+      yield address
+
+    for address in sorted(ipaddress.collapse_addresses(tor_exit_policy_reject_ipv6)):
+      # Some DNS entries resolve to invalid addresses. Filter some of them out.
+      if address.is_multicast or address.is_private or address.is_reserved or address.is_loopback or address.is_link_local:
+        continue
+      yield address
+
+  for address in rejected_addresses():
+    if isinstance(address, ipaddress.IPv4Network):
+      print('reject {address}:*'.format(address=address))
+    elif isinstance(address, ipaddress.IPv6Network):
+      print('reject6 [{address}]/{mask}:*'.format(address=address.network_address, mask=address.prefixlen))
+    else:
+      raise TypeError("Unknown address type: {address}".format(address=address))
 
